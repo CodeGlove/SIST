e3faf477f9a2214624e65fc88ff80dc229c9ee24
garamheo <gahramh@gmail.com>
2025-07-22 17:24:01 +0900

    07.22
    학원

---
+++ Ex0722_Bbs_MVC/src/main/java/bbs/action/SaveImgAction.java
@@ -0,0 +1,39 @@
+package bbs.action;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+
+public class SaveImgAction implements Action{
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        //이미지들이 저장될 위치를 절대경로로 준비해야 한다.
+        ServletContext application = request.getServletContext();
+        String realPath = application.getRealPath("editor_img");
+
+        //첨부되어오는 이미지 파일을 위에서 준비한 절대경로에 업로드 시키기 위해서다.
+        try{
+            MultipartRequest mr = new MultipartRequest(request, realPath,
+                    1024*1024*5, "utf-8", new DefaultFileRenamePolicy());
+                //이때 파일은 이미 editor_img폴더에 업로드가 된 상태다.
+
+                //파일이 저장될 때 이름이 변경될 수 있기 때문에 저장된 파일의
+                //정확한 이름을 알아내야 한다.
+            File f = mr.getFile("upload");
+            String f_name = null;
+            if(f != null){
+                f_name = f.getName();
+
+                request.setAttribute("f_name", f_name);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "saveImg.jsp";
+    }
+}
